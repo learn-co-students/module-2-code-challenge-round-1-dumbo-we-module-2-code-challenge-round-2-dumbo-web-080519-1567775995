001ed97cd107d4a44ecfc65f9e8ad3295c29ff2a
Renée Cruz <reneebasscruz@gmail.com>
2019-09-06 11:03:20 -0400

    done

---
+++ app/controllers/appearances.rb
@@ -0,0 +1,24 @@
+class AppearancesController < ApplicationController
+
+  def new
+    @appearance = Apperance.new(flash[:attributes])
+  end
+
+  def create
+    @appearance = Appearance.create(appearance_params)
+    if @appearance.valid?
+      redirect_to @appearance
+    else 
+      flash[:errors] = @appearance.errors.full_messages
+      flash[:attributes] = @appearance.attributes
+      redirect_to new_appearance_path
+    end
+  end
+
+  private
+
+  def appearance_params
+    params.require(:appearance).permit(:guest, :episode, :guest_rating)
+  end
+
+end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,9 @@
 class Guest < ApplicationRecord
 
+  has_many :appearances
+  has_many :guests, through: :appearances
+
+  validates :name, :occupation, presence: true
+  validates :name, uniqueness: true
+
 end
